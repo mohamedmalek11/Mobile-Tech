2bae095429555480a5bed75167b1759f7f117ea0
mohamedmalek11 <mohamad.sh.msm@gmail.com>
2021-07-16 02:47:37 +0200

    Retouched Category page

--- lib/pages/categories.dart
+++ lib/pages/categories.dart
@@ -23,9 +23,198 @@ class CategoriesState extends State<Categories> {
         ),
         drawer: AppDrawer(),
         body: Container(
+          padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
           child: GridView(
-            gridDelegate: null,
-            children: [],
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 15,
+              childAspectRatio: 0.8,
+            ),
+            children: [
+              InkWell(
+                child: Container(
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.symmetric(vertical: 5),
+                  child: Card(
+                      child: Column(
+                    children: [
+                      Expanded(
+                          child: Image.asset(
+                        "assets/images/brands/apple.png",
+                      )),
+                      Container(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+                        child: RichText(
+                            text: TextSpan(
+                          text: 'APPLE',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold),
+                        )),
+                      )
+                    ],
+                  )),
+                ),
+              ),
+              InkWell(
+                child: Container(
+                  margin: EdgeInsets.symmetric(vertical: 5),
+                  child: Card(
+                      child: Column(
+                    children: [
+                      Expanded(
+                          child: Image.asset(
+                        "assets/images/brands/huawei.png",
+                      )),
+                      Container(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+                        child: RichText(
+                            text: TextSpan(
+                          text: 'HUAWEI',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold),
+                        )),
+                      )
+                    ],
+                  )),
+                ),
+              ),
+              InkWell(
+                child: Container(
+                  margin: EdgeInsets.symmetric(vertical: 5),
+                  child: Card(
+                      child: Column(
+                    children: [
+                      Expanded(
+                          child: Image.asset(
+                        "assets/images/brands/nokia.png",
+                      )),
+                      Container(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+                        child: RichText(
+                            text: TextSpan(
+                          text: 'NOKIA',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold),
+                        )),
+                      )
+                    ],
+                  )),
+                ),
+              ),
+              InkWell(
+                child: Container(
+                  margin: EdgeInsets.symmetric(vertical: 5),
+                  child: Card(
+                      child: Column(
+                    children: [
+                      Expanded(
+                          child: Image.asset(
+                        "assets/images/brands/oppo.png",
+                      )),
+                      Container(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+                        child: RichText(
+                            text: TextSpan(
+                          text: 'OPPO',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold),
+                        )),
+                      )
+                    ],
+                  )),
+                ),
+              ),
+              InkWell(
+                child: Container(
+                  margin: EdgeInsets.symmetric(vertical: 5),
+                  child: Card(
+                      child: Column(
+                    children: [
+                      Expanded(
+                          child: Image.asset(
+                        "assets/images/brands/samsung.png",
+                      )),
+                      Container(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+                        child: RichText(
+                            text: TextSpan(
+                          text: 'SAMSUNG',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold),
+                        )),
+                      )
+                    ],
+                  )),
+                ),
+              ),
+              InkWell(
+                child: Container(
+                  margin: EdgeInsets.symmetric(vertical: 5),
+                  child: Card(
+                      child: Column(
+                    children: [
+                      Expanded(
+                          child: Image.asset(
+                        "assets/images/brands/vivo.png",
+                      )),
+                      Container(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+                        child: RichText(
+                            text: TextSpan(
+                          text: 'VIVO',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold),
+                        )),
+                      )
+                    ],
+                  )),
+                ),
+              ),
+              InkWell(
+                child: Container(
+                  margin: EdgeInsets.symmetric(vertical: 5),
+                  child: Card(
+                      child: Column(
+                    children: [
+                      Expanded(
+                          child: Image.asset(
+                        "assets/images/brands/xiaomi.png",
+                      )),
+                      Container(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+                        child: RichText(
+                            text: TextSpan(
+                          text: 'XIAOMI',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold),
+                        )),
+                      )
+                    ],
+                  )),
+                ),
+              ),
+            ],
           ),
         ),
       ),
