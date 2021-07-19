99e20163303dd049b03c1b9a11d22b475044b110
mohamedmalek11 <mohamad.sh.msm@gmail.com>
2021-07-19 04:04:18 +0200

    Changed fonts

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'MobileTech',
+      theme: ThemeData(fontFamily: "Almarai"),
       home: Home(),
       routes: {
         "categories": (context) {
